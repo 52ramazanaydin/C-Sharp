c184d779595442168bc14a233bbf0db722ca991d
52ramazanaydin <44252862+52ramazanaydin@users.noreply.github.com>
2022-08-11 15:59:11 +0300

    Add files via upload

---
+++ Öğrenci Bilgileri Menüsü/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*              1 - Öğrenci bilgilerini göster .
+                2 - Öğrenci ortalamasını göster .
+                3 - Öğrenci okulunu göster .
+                4 - Çıkış yap .
+ * 
+ */
+
+namespace ÖğrenciBilgileriMenüsü
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            bool kontrol = true;
+            Ogrenci ogrenci1 = new Ogrenci(123231, "Ramazan", "Aydın", 54, 65, 72, "İlkokul");
+
+            Console.WriteLine("Uygulamamıza Hoşgeldiniz ... Yapmak istedğiniz işlemi seçin  : ");
+
+            while (kontrol)
+            {
+                islemleriGoster();
+                string secim = Console.ReadLine();
+                Console.WriteLine("*************************************");
+
+                switch (secim)
+                {
+                    case "1":
+                        ogrenci1.OgrenciBilgileriGoster();
+                        break;
+                    case "2":
+                        double ogrenciOrtalama = ogrenci1.ogrencinotuBul();
+                        Console.WriteLine("Öğrenci ortalamasını göster : " + ogrenciOrtalama);
+                        break;
+                    case "3":
+                        ogrenci1.okulGetir();
+                        break;
+                    case "4":
+                        kontrol = false;
+                        break;
+                        Console.WriteLine("*************************************");
+
+                }
+
+
+            }
+
+
+            static void islemleriGoster()
+
+            {
+                Console.WriteLine("1 - Öğrenci bilgilerini göster .");
+                Console.WriteLine("2 - Öğrenci ortalamasını göster .");
+                Console.WriteLine("3 - Öğrenci okulunu göster .");
+                Console.WriteLine("4 - Çıkış yap .");
+            }
+
+        }
+
+    }
+}
---
+++ Öğrenci Bilgileri Menüsü/Öğrenci.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ÖğrenciBilgileriMenüsü
+{
+    public class Ogrenci
+    {
+        private int ogrenciNo;
+        private string isim;
+        private string soyisim;
+        private int Vize1;
+        private int Vize2;
+        private int Final;
+        private string Okulismi;
+
+        public Ogrenci(int _ogrenciNo, string _isim, string _soyisim, int _Vize1, int _Vize2, int _Final, string _Okulismi)
+        {
+            ogrenciNo = _ogrenciNo;
+            isim = _isim;
+            soyisim = _soyisim;
+            Vize1 = _Vize1;
+            Vize2 = _Vize2;
+            Final = _Final;
+            Okulismi = _Okulismi;
+
+
+
+
+        }
+        public void OgrenciBilgileriGoster()
+        {
+            Console.WriteLine("Öğrencinin Numarası : " + ogrenciNo);
+            Console.WriteLine("Öğrencinin İsmi : " + isim);
+            Console.WriteLine("Öğrencinin Soyismi : " + soyisim);
+            Console.WriteLine("Öğrencinin Vize 1  : " + Vize1);
+            Console.WriteLine("Öğrencinin Vize 2 : " + Vize2);
+            Console.WriteLine("Öğrencinin Finali  : " + Final);
+            Console.WriteLine("Öğrencinin Okul ismi : " + Okulismi);
+
+        }
+
+        public double ogrencinotuBul()
+        {
+            double ortalama = Vize1 * 0.2 + Vize2 * 0.2 + Final * 0.6;
+
+            return ortalama;
+        }
+
+        public void okulGetir()
+        {
+            Console.WriteLine("Okulun İsmi" + Okulismi);
+        }
+    }
+}
