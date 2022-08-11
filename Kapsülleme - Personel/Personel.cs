a9d5ff3e3e6cfa64fdb8b27e5b76a8801ca83569
52ramazanaydin <44252862+52ramazanaydin@users.noreply.github.com>
2022-08-11 16:06:48 +0300

    Add files via upload

---
+++ Kapsülleme - Personel/Personel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kapsülleme
+{
+    public class Personel
+    {
+        private string tckimlikno;
+
+        public string TCKİMLİKNO
+        {
+            get
+            {
+                return tckimlikno.Substring(0, 5) + "******";
+            }
+
+            set
+            {
+                bool kontrol = false;
+
+                if (value.Length == 11)
+                {
+                    for (int i = 0; i < value.Length; i++)
+                    {
+                        bool sayimi = char.IsNumber(value[i]);
+                        if (sayimi)
+                        {
+                            //Yapmamız gereken birşey yok
+                        }
+                        else
+                        {
+                            kontrol = true;
+                            break;
+                        }
+                    }
+                    if (kontrol)
+                    {
+                        Console.WriteLine("Tc kimlik numaranızda geçersiz karakter bulundu .");
+                    }
+                    else
+                    {
+                        tckimlikno = value;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Girdiğiniz tc kimlik no 11 karakter değil. ");
+                }
+            }
+        }
+
+    }
+
+}
---
+++ Kapsülleme - Personel/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Kapsülleme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personel P1 = new Personel();
+            P1.TCKİMLİKNO = "12345678910";
+            Console.WriteLine("Tc Kimlik numaranız : " + P1.TCKİMLİKNO);
+
+        }
+    }
+
+}
