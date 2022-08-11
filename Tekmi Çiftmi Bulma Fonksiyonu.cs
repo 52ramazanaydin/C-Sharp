ea201c244a3182736d744838ff5dd04a8135c31f
52ramazanaydin <44252862+52ramazanaydin@users.noreply.github.com>
2022-08-11 15:05:28 +0300

    Add files via upload

---
+++ Tekmi Çiftmi Bulma Fonksiyonu.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SayınınKaresiniHesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+              
+            
+            
+        {
+            static void tekmi_ciftmi(int sayi1)
+            {
+                if (sayi1 % 2 == 0)
+                {
+                    Console.WriteLine("sayi çifttir ...");
+                }
+                else
+                {
+                    Console.WriteLine("sayi tektir ...");
+                }
+
+            }
+            Console.Write("Sayıyı girin : ");
+             int sayi = Convert.ToInt32(Console.ReadLine());
+            tekmi_ciftmi(sayi);
+
+
+        }
+
+    }
+}
