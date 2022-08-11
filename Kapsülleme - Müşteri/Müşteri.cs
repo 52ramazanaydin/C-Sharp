e67846acb3b66372812a9234553eeabed1f17c4c
52ramazanaydin <44252862+52ramazanaydin@users.noreply.github.com>
2022-08-11 16:04:30 +0300

    Add files via upload

---
+++ Kapsülleme - Müşteri/Müşteri.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kapsülleme
+{
+    public class Musteri
+    {
+        private int kilo;
+        public int KİLO
+        {
+            get
+            {
+                return kilo;
+            }
+            set
+            {
+                kilo = value;
+            }
+        }
+    }
+
+}
---
+++ Kapsülleme - Müşteri/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Kapsülleme
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Property(Özellik)
+            Musteri musteri1 = new Musteri();
+            musteri1.KİLO = 85;  // Değer ataması var o yüzden set çalışır.
+
+            Console.WriteLine(musteri1.KİLO); // get çalışır.
+        }
+    }
+
+}
