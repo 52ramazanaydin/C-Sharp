83dd4c32187dfda03dd32f9b55f703c14b1171db
52ramazanaydin <44252862+52ramazanaydin@users.noreply.github.com>
2022-08-11 14:54:01 +0300

    Add files via upload

---
+++ Return Statik Fonksiyonlar.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ÇiftSayıTekSayıKontrolü
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int donendeger = toplamDondur(5, 12);
+            Console.WriteLine(donendeger);
+            string Donenisim = isimSoyisimDondur("Ramazan..", "Aydın..");
+            Console.WriteLine(Donenisim);
+
+            static int toplamDondur(int x, int y)
+            {
+                int sonuc = x + y;
+                return sonuc;
+            }
+
+            static string isimSoyisimDondur(string isim, string soyisim)
+            {
+
+                return isim + " " + soyisim;
+            }
+
+
+
+        }
+
+    }
+}
