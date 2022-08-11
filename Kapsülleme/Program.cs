691f37b787e0151a794ee38c992a0d3a92c9f6bf
52ramazanaydin <44252862+52ramazanaydin@users.noreply.github.com>
2022-08-11 16:02:45 +0300

    Add files via upload

---
+++ Kapsülleme/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Kapsülleme
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kapsulleme get ve set metotları ile yapılır ya da property ile yapılır .
+
+            Ogrenci ogrenci1 = new Ogrenci();
+            ogrenci1.setIsim("Ramazan");
+            Console.WriteLine(ogrenci1.getIsim());
+        }
+    }
+}
