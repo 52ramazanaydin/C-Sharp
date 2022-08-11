314f39562b1f58bba1c1d48ca5851eb94a96ede9
52ramazanaydin <44252862+52ramazanaydin@users.noreply.github.com>
2022-08-11 14:41:06 +0300

    Add files via upload

---
+++ Statik Fonksiyonlar.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ÇiftSayıTekSayıKontrolü
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SelamVer();
+
+            static void SelamVer()
+            {
+                Console.WriteLine("Selamlar :))");
+            }
+
+            static void AdiniYazdir()
+            {
+                Console.WriteLine("Ramazan AYDIN...");
+            }
+
+            AdiniYazdir();
+
+
+        }
+
+    }
+}
