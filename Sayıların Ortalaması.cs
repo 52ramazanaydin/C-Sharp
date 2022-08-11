f47b353906f94ae4de8c5f0d4b83dacaa76487ac
52ramazanaydin <44252862+52ramazanaydin@users.noreply.github.com>
2022-08-11 14:29:13 +0300

    Add files via upload

---
+++ Sayıların Ortalaması.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OrtalamaHesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kullanıcıdan sayı al 0 dan aldığımız sayıya kadar topla 
+            //  girdiği sayıya böl. Ortalama 50den büyükse BÜYÜK DEğilse KÜÇÜK yaz
+
+            Console.WriteLine("Sayıyı girin :");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            double ortalama = 0;
+            int topla = 0;
+
+            for (int i = 0; i < sayi; i++)
+            {
+                topla += i;
+            }
+
+
+            ortalama = topla / sayi;
+            if (ortalama > 50)
+            {
+                Console.WriteLine("Ortalama 50'den büyüktür ..");
+                Console.WriteLine("Ortalama : " + ortalama);
+            }
+            else
+            {
+
+                Console.WriteLine("Ortalama 50'den küçüktür ..");
+                Console.WriteLine("Ortalama : " + ortalama);
+            }
+        }
+
+    }
+}
