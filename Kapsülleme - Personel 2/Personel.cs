8621af51777c26b022f77d53d17b158b42a38164
52ramazanaydin <44252862+52ramazanaydin@users.noreply.github.com>
2022-08-11 16:08:58 +0300

    Add files via upload

---
+++ Kapsülleme - Personel 2/Personel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kapsülleme
+{
+    public class Personel
+    {
+        public string name;
+        public int id;
+        public string surname;
+
+        public static int yas;
+
+
+        public static void isimSoyisimYaz()
+        {
+            Console.WriteLine("Ramazan AYDIN..");
+        }
+
+        public static void topla(int sayi1, int sayi2)
+        {
+            Console.WriteLine("iki sayinin toplamı : " + (sayi1 + sayi2));
+        }
+    }
+}
---
+++ Kapsülleme - Personel 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Kapsülleme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Personel.isimSoyisimYaz();
+            Personel.topla(89, 85);
+
+            Personel p1 = new Personel();
+            p1.id = 123321;
+            p1.name = "ramo";
+            p1.surname = "asdasda";
+            Personel.yas = 23;
+
+            Console.WriteLine("id :" + p1.id);
+            Console.WriteLine("id :" + p1.name);
+            Console.WriteLine("id :" + p1.surname);
+            Console.WriteLine("id :" + Personel.yas);
+        }
+    }
+
+}
