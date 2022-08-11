7d98e91e706cb0c8c8a90329d615069ec3d9cae3
52ramazanaydin <44252862+52ramazanaydin@users.noreply.github.com>
2022-08-11 15:07:26 +0300

    Add files via upload

---
+++ Dizinin Min Elemanını Bulma.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DizininMinElemanınıBulma
+{
+    class Program
+    {
+        static void Main(string[] args)
+                     
+        {
+            int[] sayidizisi = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 12312, 6756756, -15 };
+            Console.WriteLine(Min(sayidizisi));
+            static int Min(int[] sayilar)
+            {
+                int kucuk = sayilar[0];
+
+                for (int i = 0; i < sayilar.Length; i++)
+                {
+                    if (kucuk > sayilar[i])
+                    { kucuk = sayilar[i]; }
+                }
+                return kucuk;
+            }
+
+
+
+        }
+
+    }
+}
