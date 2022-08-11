d192c73b9a538b7fd8df4931b7e11e472b3136cd
52ramazanaydin <44252862+52ramazanaydin@users.noreply.github.com>
2022-08-11 15:15:33 +0300

    Add files via upload

---
+++ ConstructorMethod/Araba.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorMethod
+{
+    public class Araba
+    {
+        public int kapiSayisi;
+        public string arabaModel;
+        public string arabaRengi;
+
+        public Araba()//Constructor method
+        {
+            //ctor tab tab ile olusturulabilir.
+            Console.WriteLine("Constructor method çalıştırılıyor..");
+        }
+
+        public void MotorCalistir()
+        {
+            Console.WriteLine("Motor Çalıştırılıyorr...!!");
+        }
+
+        public void kapilarKilitle()
+        {
+            Console.WriteLine("Kapılar Kilitleniyor..!");
+
+
+        }
+    }
+}
---
+++ ConstructorMethod/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConstructorMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+                     
+        {
+            Araba araba1 = new Araba();
+
+            araba1.kapiSayisi = 4;
+            araba1.arabaRengi = "Mavi";
+            araba1.arabaModel = "Honda";
+
+            araba1.MotorCalistir();
+            araba1.kapilarKilitle();
+
+            Console.WriteLine("Arabanın kapı sayısı : " + araba1.kapiSayisi);
+            Console.WriteLine("Arabanın rengi : " + araba1.arabaRengi);
+            Console.WriteLine("Arabanın modeli : " + araba1.arabaModel);
+
+
+
+
+        }
+
+    }
+}
