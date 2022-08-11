ec56c621c40edb895911d84b10695b0116e756be
52ramazanaydin <44252862+52ramazanaydin@users.noreply.github.com>
2022-08-11 16:10:24 +0300

    Add files via upload

---
+++ Kapsülleme - Personel 3/Personel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kapsülleme
+{
+    public class Personel
+    {
+        public int Personelid { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public static int maas { get; set; }
+
+        static Personel()
+        {
+            maas = 2000;
+
+        }
+        public Personel(int _Personelid, string _Name, string _Surname)
+        {
+            Personelid = _Personelid;
+            Name = _Name;
+            Surname = _Surname;
+
+
+        }
+
+        public void bilgileriGoster()
+        {
+            Console.WriteLine("Kullanıcı bilgiler");
+            Console.WriteLine("id : " + Personelid);
+            Console.WriteLine("name : " + Name);
+            Console.WriteLine("Surname : " + Surname);
+            Console.WriteLine("Maas : " + maas);
+
+        }
+        public void ZamYap(int zamMiktari)
+        {
+            Console.WriteLine("Yeni maaş" + (maas + zamMiktari));
+        }
+
+    }
+}
---
+++ Kapsülleme - Personel 3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Kapsülleme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Personel p1 = new Personel(123321, "ramazannnn", "aydınnn");
+
+            p1.bilgileriGoster();
+            p1.ZamYap(1000);
+
+        }
+    }
+
+
+}
